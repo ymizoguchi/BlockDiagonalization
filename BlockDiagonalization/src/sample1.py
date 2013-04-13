9f285162c323f78a66be421b6865bac221abcc1e
ymizoguchi <ym@imi.kyushu-u.ac.jp>
2013-04-14 04:45:14 +0900

    cmath

--- BlockDiagonalization/src/sample1.py
+++ BlockDiagonalization/src/sample1.py
@@ -3,12 +3,20 @@ Created on 2013/04/14
 
 @author: ym
 '''
-import numpy
-from  scipy import *
-import BlockDiagonalization as Bd
+from math import *
+from cmath import *
+import numpy as np
+import scipy as sp
+# import BlockDiagonalization as Bd
 
 if __name__ == '__main__':
-    a = array([[1,2],[3,4]])
-    b = array([[5,6],[7,8]])
-    print Bd.mydot(a,b)
-    print dot(a,b)
+    pi=3.141592
+    a = np.matrix([[cos(pi/3),-sin(pi/3)],[sin(pi/3),cos(pi/3)]])
+    b = np.linalg.eig(a)
+#    print Bd.mydot(a,b)
+#    print dot(a,b)
+    print a
+    print b[0]
+    print b[1]
+    print b[1][0,1]
+    print phase(b[1][0,1])/pi
